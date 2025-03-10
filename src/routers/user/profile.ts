ef87392ac84ddd0d64f01c184bb7d8fc253e8609
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-10 15:33:51 +0500

    Add profile management routes for admin, user, and vendor

--- src/routers/user/profile.ts
+++ src/routers/user/profile.ts
@@ -1,5 +1,42 @@
 import { Router } from "express";
 
+import {
+  createProfile,
+  getProfile,
+  updateProfile,
+} from "~/controllers/profile";
+import { verifyRequest } from "~/middlewares/auth";
+import { uploadOne } from "~/middlewares/upload";
+
 const profileRouter = Router();
+
+profileRouter.get(
+  "/",
+  verifyRequest({
+    allowedTypes: ["ACCESS"],
+    isVerified: true,
+  }),
+  getProfile,
+);
+
+profileRouter.post(
+  "/",
+  verifyRequest({
+    allowedTypes: ["ACCESS"],
+    isVerified: true,
+  }),
+  uploadOne("picture"),
+  createProfile,
+);
+
+profileRouter.put(
+  "/",
+  verifyRequest({
+    allowedTypes: ["ACCESS"],
+    isVerified: true,
+  }),
+  uploadOne("picture"),
+  updateProfile,
+);
 
 export { profileRouter };
