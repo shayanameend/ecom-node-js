a001f0908b368f01aee7b346a6e80b98e7284e1c
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-10 20:01:07 +0500

    Add product retrieval and management functionality with validation

---
+++ src/controllers/admin/products.ts
@@ -0,0 +1,202 @@
+import type { Prisma } from "@prisma/client";
+import type { Request, Response } from "express";
+
+import { BadResponse, NotFoundResponse, handleErrors } from "~/lib/error";
+import { prisma } from "~/lib/prisma";
+import { addFile, removeFile } from "~/utils/file";
+import {
+  getProductParamsSchema,
+  getProductsQuerySchema,
+  toggleProductIsDeletedParamsSchema,
+  toggleProductIsDeletedBodySchema,
+} from "~/validators/admin/products";
+
+async function getProducts(request: Request, response: Response) {
+  try {
+    const {
+      page,
+      limit,
+      sort,
+      name,
+      minStock,
+      minPrice,
+      maxPrice,
+      isDeleted,
+      categoryId,
+      vendorId,
+    } = getProductsQuerySchema.parse(request.query);
+
+    const where: Prisma.ProductWhereInput = {};
+
+    if (name) {
+      where.name = {
+        contains: name,
+        mode: "insensitive",
+      };
+    }
+
+    if (minStock !== undefined) {
+      where.stock = {
+        gte: minStock,
+      };
+    }
+
+    if (minPrice !== undefined && maxPrice !== undefined) {
+      where.price = {
+        gte: minPrice,
+        lte: maxPrice,
+      };
+    }
+
+    if (minPrice !== undefined) {
+      where.price = {
+        gte: minPrice,
+      };
+    }
+
+    if (maxPrice !== undefined) {
+      where.price = {
+        lte: maxPrice,
+      };
+    }
+
+    if (isDeleted !== undefined) {
+      where.isDeleted = isDeleted;
+    }
+
+    if (categoryId) {
+      where.categoryId = categoryId;
+    }
+
+    if (vendorId) {
+      where.vendorId = vendorId;
+    }
+
+    const products = await prisma.product.findMany({
+      where,
+      take: limit,
+      skip: (page - 1) * limit,
+      orderBy: {
+        ...(sort === "POPULARITY" && {
+          orderToProduct: { _count: "desc" },
+        }),
+        ...(sort === "LATEST" && { createdAt: "desc" }),
+        ...(sort === "OLDEST" && { createdAt: "asc" }),
+      },
+      select: {
+        id: true,
+        pictureIds: true,
+        name: true,
+        description: true,
+        sku: true,
+        stock: true,
+        price: true,
+        salePrice: true,
+        isDeleted: true,
+        categoryId: true,
+        vendorId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    const total = await prisma.product.count({ where });
+    const pages = Math.ceil(total / limit);
+
+    return response.success(
+      {
+        data: { products },
+        meta: { total, pages, limit, page },
+      },
+      {
+        message: "Products fetched successfully!",
+      },
+    );
+  } catch (error) {
+    handleErrors({ response, error });
+  }
+}
+
+async function getProduct(request: Request, response: Response) {
+  try {
+    const { id } = getProductParamsSchema.parse(request.params);
+
+    const product = await prisma.product.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        pictureIds: true,
+        name: true,
+        description: true,
+        sku: true,
+        stock: true,
+        price: true,
+        salePrice: true,
+        isDeleted: true,
+        categoryId: true,
+        vendorId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundResponse("Product not found!");
+    }
+
+    return response.success(
+      {
+        data: { product },
+      },
+      {
+        message: "Product fetched successfully!",
+      },
+    );
+  } catch (error) {
+    handleErrors({ response, error });
+  }
+}
+
+async function toggleProductIsDeleted(request: Request, response: Response) {
+  try {
+    const { id } = toggleProductIsDeletedParamsSchema.parse(request.params);
+    const validatedData = toggleProductIsDeletedBodySchema.parse(request.body);
+
+    const product = await prisma.product.update({
+      where: { id },
+      data: validatedData,
+      select: {
+        id: true,
+        pictureIds: true,
+        name: true,
+        description: true,
+        sku: true,
+        stock: true,
+        price: true,
+        salePrice: true,
+        isDeleted: true,
+        categoryId: true,
+        vendorId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundResponse("Product not found!");
+    }
+
+    return response.success(
+      {
+        data: { product },
+      },
+      {
+        message: "Product updated successfully!",
+      },
+    );
+  } catch (error) {
+    handleErrors({ response, error });
+  }
+}
+
+export { getProducts, getProduct, toggleProductIsDeleted };
--- src/routers/admin/products.ts
+++ src/routers/admin/products.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
 
+import {
+  getProduct,
+  getProducts,
+  toggleProductIsDeleted,
+} from "~/controllers/admin/products";
+
 const productsRouter = Router();
+
+productsRouter.get("/", getProducts);
+
+productsRouter.get("/:id", getProduct);
+
+productsRouter.put("/:id", toggleProductIsDeleted);
 
 export { productsRouter };
