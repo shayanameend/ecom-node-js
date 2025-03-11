5e5ebb4647882d4039d61783ca61e50a205c2758
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-11 08:10:38 +0500

    Add CRUD routes for orders in admin, user, and vendor routers

--- src/routers/user/orders.ts
+++ src/routers/user/orders.ts
@@ -2,4 +2,12 @@ import { Router } from "express";
 
 const ordersRouter = Router();
 
+ordersRouter.get("/");
+
+ordersRouter.get("/:id");
+
+ordersRouter.post("/");
+
+ordersRouter.put("/:id");
+
 export { ordersRouter };
--- src/routers/vendor/orders.ts
+++ src/routers/vendor/orders.ts
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const ordersRouter = Router();
 
+ordersRouter.get("/");
+
+ordersRouter.get("/:id");
+
+ordersRouter.put("/:id");
+
 export { ordersRouter };
